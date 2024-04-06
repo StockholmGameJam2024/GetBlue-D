d1769a8dbb4c9ae2e01810975d48eb554e01248a
Branko Zikic <branko.zikic@student.forsbergsskola.se>
2024-04-07 01:31:23 +0200

    Deleted the prefab for the player and added the prefab of the other player in the scene

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -1,26 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerSpawner : MonoBehaviour
 {
-    public Player[] players;
+    public List<Player> players;
     public int playerNumber = 4;
     public Player playerPrefab;
     public Transform[] spawnPositions;
     public Image[] playerHUDs;
-    
+    private Color[] colors;
+
     // Start is called before the first frame update
     void Start()
     {
-        var colors = HueHelper.GenerateEvenlyDistributedColors(4);
-        players = new Player[playerNumber];
-        for (var i = 0; i < players.Length; i++)
-        {
-            players[i] = Instantiate(playerPrefab, spawnPositions[i].position, Quaternion.identity);
-            players[i].CurrentColor = colors[i];
-            Color.RGBToHSV(colors[i], out var hue, out var s, out var v);
-            players[i].targetColor = Color.HSVToRGB((hue + 0.5f) % 1f, s, v);
-            playerHUDs[i].color = players[i].targetColor;
-        }
+        colors = HueHelper.GenerateEvenlyDistributedColors(GetComponent<PlayerInputManager>().maxPlayerCount);
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        int i = players.Count;
+        players.Add(playerInput.GetComponent<Player>());
+        players[i].CurrentColor = colors[i];
+        Color.RGBToHSV(colors[i], out var hue, out var s, out var v);
+        players[i].targetColor = Color.HSVToRGB((hue + 0.5f) % 1f, s, v);
+        playerHUDs[i].color = players[i].targetColor;
+    }
+
+    public void OnPlayerLeft(PlayerInput playerInput)
+    {
+        
     }
 }
