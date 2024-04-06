19f61e94733c24d8bb9d8e4c63f9a3b3cd8c5705
Mitchell Hentges <mitch9654@gmail.com>
2024-04-06 18:48:51 +0200

    Adds "spring prediction" line

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,16 @@ using UnityEngine.Serialization;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float acceleration;
+    private float maxTension;
+    [SerializeField]
+    private float tensionAcceleration;
+    [SerializeField]
+    private float tensionFrictionDeceleration;
     private Vector2 _tension;
     private InputMaster _playerActions;
     private Rigidbody2D _rbody;
     private Vector2 _moveInput;
+    public LineRenderer lineRenderer;
 
     void Awake()
     {
@@ -35,7 +40,20 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         _moveInput = _playerActions.Player_Map.Movement.ReadValue<Vector2>();
-        _tension += _moveInput * acceleration;
+        _tension += _moveInput * tensionAcceleration;
+        _tension *= 1 - tensionFrictionDeceleration;
+
+        var color = Color.red;
+        color.a = 1f;
+        lineRenderer.transform.position = _rbody.position;
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+        lineRenderer.positionCount = 2;
+        var vector = Vector3.zero;
+        vector.x += _tension.x * 0.1f;
+        vector.y += _tension.y * 0.1f;
+        lineRenderer.SetPosition(0, Vector3.zero);
+        lineRenderer.SetPosition(0, vector);
     }
 
     void OnSpring()
